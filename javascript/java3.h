4955d109478cca5ae80f6ba0b403a5a5592fa4f6
AMAL MANOJ <145776188+maayavii@users.noreply.github.com>
2023-11-02 12:37:09 +0530

    Add files via upload

---
+++ javascript/java3.h
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>
+h1{color:green;
+font:bold;}
+</style>
+</head>
+<body>
+<h1>current date</h1>
+
+<p id="date"></p>
+
+<script>
+function printdate(){
+var X =new date();
+document.get
+</script>
+</body>
+</html>
